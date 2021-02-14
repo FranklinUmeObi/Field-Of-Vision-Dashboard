ceec90e91f5aa30b6e47c936f6d0a3be66e5bdde
Franklin Ume Obiekwe <franklinumeobi@gmail.com>
2021-02-14 16:20:51 +0000

    Organised Routing

--- fov-webapp/src/App.js
+++ fov-webapp/src/App.js
@@ -1,11 +1,19 @@
+import "./App.css";
 
-import './App.css';
+import { HashRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>The web app runs :)</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/">
+          <h1>Our WebApp Works</h1>
+        </Route>
+        <Route exact path="/test">
+          <h1>Router Works</h1>
+        </Route>
+      </div>
+    </Router>
   );
 }
 
