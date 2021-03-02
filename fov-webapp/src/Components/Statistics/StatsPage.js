1c39aa6ecc035d60f45fb48c340a300a6992373f
Franklin Ume Obiekwe <franklinumeobi@gmail.com>
2021-03-02 22:09:05 +0000

    Added dummy Stats components

--- fov-webapp/src/Components/Statistics/StatsPage.js
+++ fov-webapp/src/Components/Statistics/StatsPage.js
@@ -1,17 +1,82 @@
-import React from 'react'
+import React from "react";
 import "./StatsPage.css";
+import Chart from "react-apexcharts";
 
 function StatsPage() {
-    return (
-        <div className="statsAdmin">
-            <h1>Stats</h1>
-            <h1>1</h1>
-            <h1>2</h1>
-            <p className="heightSpacer">This just lets scrolling occur</p>
-            
-            
+  let pieData = {
+    options: {
+      labels: ["Home", "Away"],
+    },
+    series: [40, 60],
+  };
+
+  let areaData = {
+    series: [{
+        name: 'Home',
+        data: [31, 40, 28, 51, 42, 109, 100]
+      }, {
+        name: 'Away',
+        data: [11, 32, 45, 32, 34, 52, 41]
+      }],
+      options:{
+        chart: {
+        type: 'area'
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'smooth'
+      },
+      xaxis: {
+        type: 'datetime',
+        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+      },
+      tooltip: {
+        x: {
+          format: 'dd/MM/yy HH:mm'
+        },
+      },
+    }
+    };
+
+  return (
+    <div className="statsAdmin">
+      <div className="statsGrid">
+
+        <div className="statCardp5">
+
         </div>
-    )
+        <div className="Statsmalls">
+            <div className="statCardSmall"></div>
+            <div className="statCardSmall"></div>
+        </div>
+
+        
+
+        <div className="statCard">
+          <h3 className="statLabel">Possesion</h3>
+          <div className="graph">
+          <Chart
+            options={pieData.options}
+            series={pieData.series}
+            type="donut"
+            width="110%"
+          />
+          </div>
+          
+        </div>
+
+        <div className="statCard">
+          <h3 className="statLabel">Distance Travelled</h3>
+          <div className="graph">
+          <Chart options={areaData.options}  series={areaData.series} type="area" width="120%" />
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default StatsPage
+export default StatsPage;
