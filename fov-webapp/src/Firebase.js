d89eacba7a7e1fa8b0c7fd8594eab373b5ebc3a9
Franklin Ume Obiekwe <franklinumeobi@gmail.com>
2021-03-13 17:37:39 +0000

    hid api keys

--- fov-webapp/src/Firebase.js
+++ fov-webapp/src/Firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 
 
 let config = {
-    apiKey: "AIzaSyChcUN8JT0Ww4EZsI_dT1WmJI4NH3TJSE8",
-    authDomain: "fov-dashboard.firebaseapp.com",
-    projectId: "fov-dashboard",
-    storageBucket: "fov-dashboard.appspot.com",
-    messagingSenderId: "985575055085",
-    appId: "1:985575055085:web:99d95d2ed37b93d2b676e4"
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 }
 
 const app = firebase.initializeApp(config);
